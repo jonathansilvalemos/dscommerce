103c3eb827a5459a188e31f1bd7419f22a6a0a30
Jonathan Lemos <jonathan.lemos@aluno.riogrande.ifrs.edu.br>
2024-12-14 14:41:12 -0300

    Criacao do projeto dscommerce

---
+++ src/main/java/com/jonathan/jslcommerce/JslcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.jonathan.jslcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JslcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JslcommerceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/jonathan/jslcommerce/entities/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.jonathan.jslcommerce.entities;
+
+public enum OrderStatus {
+    WAITING_PAYMENT, PAID, SHIPPED, DELIVERED, CANCELED;
+}
